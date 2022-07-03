f78d5d5a9b18a68d386f7b626e9751487bb394a5
madokainagaki <jo6gencho9@gmail.com>
2022-07-03 21:55:06 +0900

    Initial commit

---
+++ app/src/main/java/com/example/firebasetest/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.firebasetest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+
+//最低限でやっていく。
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val dataBaseReference = FirebaseDatabase.getInstance().reference
+        val data2 = "●●の旅"
+
+        val testRef = dataBaseReference.child(data2)
+        val data = "名前だよーん"
+        Log.d("TEST",data)
+        testRef.push().setValue(data)
+
+
+
+
+    }
+
+}
