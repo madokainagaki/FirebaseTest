119fa232722ee5d2326a09306e5e93ff3e4f54cc
madokainagaki <jo6gencho9@gmail.com>
2022-07-03 22:08:10 +0900

    なんかよくわからない

---
+++ app/src/main/java/com/example/firebasetest/Const.kt
@@ -0,0 +1,17 @@
+package com.example.photo
+
+// Firebaseにユーザの表示名を保存するパス
+const val UsersPATH = "users"
+
+// Firebaseに質問を保存するバス
+const val DiaryPATH = "diarys"
+
+const val TripPATH = "trips"
+
+const val CountPATH = "counts"
+
+// Firebaseに回答を保存するパス
+const val AnswersPATH = "answers"
+
+// Preferenceに表示名を保存する時のキー
+const val NameKEY = "name"
--- app/src/main/java/com/example/firebasetest/MainActivity.kt
+++ app/src/main/java/com/example/firebasetest/MainActivity.kt
@@ -14,15 +14,19 @@ import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDataBaseReference: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val dataBaseReference = FirebaseDatabase.getInstance().reference
+
+        mDataBaseReference = FirebaseDatabase.getInstance().reference
+        mAuth = FirebaseAuth.getInstance()
+
         val data2 = "●●の旅"
 
-        val testRef = dataBaseReference.child(data2)
+        val testRef = mDataBaseReference.child(data2)
         val data = "名前だよーん"
         Log.d("TEST",data)
         testRef.push().setValue(data)
